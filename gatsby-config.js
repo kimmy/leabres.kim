4a8729baaf851f5ecde4bb485f21bdf64b69e0cd
Kim Leabres <kbleabres@gmail.com>
2018-05-18 20:06:07 +0800

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,25 @@
 module.exports = {
   siteMetadata: {
     title: "Kim Leabres | Web Developer",
-    author: "",
-    description: "Kim Leabres' personal website"
+    description: "Kim Leabres' personal website",
+    siteUrl: "http://leabres.kim",
   },
   pathPrefix: '/',
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-119529640-1",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`
+    }
   ],
 }
